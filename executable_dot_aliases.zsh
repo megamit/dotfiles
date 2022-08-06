f57e8e75bbb6538741e856bf3600a24221e14e6d
megamit <me@mreb.uk>
2022-08-07 00:17:48 +0100

    Added apt fast

--- executable_dot_aliases.zsh
+++ executable_dot_aliases.zsh
@@ -2,7 +2,9 @@ alias zp="code \"$ZDOTDIR/.zshrc\""
 alias cmdc="cmd.exe /c"
 
 # (( $+functions[k] )) && alias ls=k
-alias ll=exa
+is_bin_in_path exa && alias ll=exa
+is_bin_in_path apt-fast && alias apt=apt-fast
 
 git config --global alias.ignore \
 '!gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}; gi'
+
